6211dba169898cf0d2bc97216c0edb6ac666b415
Elnathan Dunia <ejdunia@gmail.com>
2021-04-12 23:23:36 +0100

    Added a final function that sends the mail

--- excel assignment.py
+++ excel assignment.py
@@ -36,23 +36,24 @@ def get_grades():
     grade = [grade for grade in email_scores.values()]
     return grade
 
-# defining other variables to be used
-# email_list = [emailss for emailss in email_scores]
-# usernames =[u_name[:u_name.find("@")] for u_name in email_scores]
-# grade = [grade for grade in email_scores.values()]
+def send_message(e_mesg, subject, email_add, email, message):
+    """function to send the mail with  """
+    msg = e_mesg
+    msg['Subject'] = subject
+    msg['From'] = email_add
+    msg['To'] = email
+    msg.set_content(message)
 
-
-# # testing what ive done
-# print(email_list)
-# print(grade)
-# print(username)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_add, email_pass)
+        smtp.send_message(msg)
 
 get_email_list()
 get_grades()
 get_usernames()
 
 for (username, email, grde) in zip(get_usernames(), get_email_list(), get_grades()):
-    pass_msg = f"Hi {username},\n congratulations, your score in the last test was {grde}, therefore you have qualified for the final stage. \nall the best"
+    pass_msg = f"Hi {username},\n this message was sent as a result of a refactor test {grde}, therefore..."
 
     fail_msg = f"Dear {username} I am sorry to inform you that you scored {grde} in your interview test which is less than the pass mark,\nyour journey ends with us here and we wish you all the best"
 
@@ -64,15 +65,7 @@ for (username, email, grde) in zip(get_usernames(), get_email_list(), get_grades
             draft = pass_msg
         return draft
 
-    
 
-    msg = EmailMessage()
-    msg['Subject'] = 'Python class test mail'
-    msg['From'] = email_add
-    msg['To'] = email
-    msg.set_content(check_draft())
+    send_message(e_mesg=EmailMessage(), subject="Python Test message refactor test", email_add=email_add, email=email, message=check_draft())
+    # send_message("Python test", EmailMessage(), email_add, email, check_draft())
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(email_add, email_pass)
-        smtp.send_message(msg)
-        
